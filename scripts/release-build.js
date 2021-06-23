ef833bbde6c176ffb4801269ff23ecc59b55a761
Levminer <33373714+Levminer@users.noreply.github.com>
2021-06-24 00:21:41 +0200

    Fix build number generation

--- scripts/release-build.js
+++ scripts/release-build.js
@@ -1,14 +1,7 @@
 const fs = require("fs")
 
-const build = new Date()
-	.toISOString()
-	.replace("T", "X")
-	.replaceAll(":", ".")
-	.substring(0, 19)
-	.replaceAll("-", ".")
-	.slice(2)
-	.replaceAll(".", "")
-	.replace("X", ".")
+// prettier-ignore
+const build = new Date().toISOString().replace("T", "X").replaceAll(":", ".").substring(0, 19).replaceAll("-", ".").slice(2).replaceAll(".", "").replace("X", ".")
 
 const file = JSON.parse(fs.readFileSync("package.json", "utf-8"))
 
