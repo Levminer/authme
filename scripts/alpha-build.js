a75d82a58022912b4982284f67467a65ab2f5625
Levminer <33373714+Levminer@users.noreply.github.com>
2021-06-24 15:06:58 +0200

    Final workflows

--- scripts/alpha-build.js
+++ scripts/alpha-build.js
@@ -20,10 +20,18 @@ if (!fs.existsSync("dist")) {
 
 if (fs.existsSync("build")) {
 	fs.rmdirSync("build", { recursive: true })
+	fs.mkdirSync("build")
 } else {
 	fs.mkdirSync("build")
 }
 
 fs.writeFileSync("build.json", JSON.stringify(file, null, "\t"))
 fs.writeFileSync(`dist/${file.number}.json`, JSON.stringify(file, null, "\t"))
-fs.writeFileSync(`build/${file.number}.json`, JSON.stringify(file, null, "\t"))
+
+if (process.platform === "win32") {
+	fs.writeFileSync(`build/authme-${file.number}-windows.json`, JSON.stringify(file, null, "\t"))
+} else if (process.platform === "darwin") {
+	fs.writeFileSync(`build/authme-${file.number}-mac.json`, JSON.stringify(file, null, "\t"))
+} else {
+	fs.writeFileSync(`build/authme-${file.number}-linux.json`, JSON.stringify(file, null, "\t"))
+}
--- scripts/release-build.js
+++ scripts/release-build.js
@@ -18,5 +18,20 @@ if (!fs.existsSync("dist")) {
 	fs.mkdirSync("dist")
 }
 
+if (fs.existsSync("build")) {
+	fs.rmdirSync("build", { recursive: true })
+	fs.mkdirSync("build")
+} else {
+	fs.mkdirSync("build")
+}
+
 fs.writeFileSync("build.json", JSON.stringify(file, null, "\t"))
 fs.writeFileSync(`dist/${file.number}.json`, JSON.stringify(file, null, "\t"))
+
+if (process.platform === "win32") {
+	fs.writeFileSync(`build/authme-${file.number}-windows.json`, JSON.stringify(file, null, "\t"))
+} else if (process.platform === "darwin") {
+	fs.writeFileSync(`build/authme-${file.number}-mac.json`, JSON.stringify(file, null, "\t"))
+} else {
+	fs.writeFileSync(`build/authme-${file.number}-linux.json`, JSON.stringify(file, null, "\t"))
+}
