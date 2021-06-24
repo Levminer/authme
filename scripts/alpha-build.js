e11daea69b5d0a64875133f303836988dbedb66c
Levminer <33373714+Levminer@users.noreply.github.com>
2021-06-24 15:45:23 +0200

    Last workflow update

--- scripts/alpha-build.js
+++ scripts/alpha-build.js
@@ -13,8 +13,12 @@ const build = new Date()
 const build_dot = `alpha.${build}`
 const build_hyphen = `alpha-${build.replaceAll(".", "-")}`
 
-const file = {
+const file_dot = {
 	number: build_dot,
+}
+
+const file_hyphen = {
+	number: build_hyphen,
 }
 
 if (!fs.existsSync("dist")) {
@@ -28,13 +32,14 @@ if (fs.existsSync("build")) {
 	fs.mkdirSync("build")
 }
 
-fs.writeFileSync("build.json", JSON.stringify(file, null, "\t"))
-fs.writeFileSync("dist/build.json", JSON.stringify(file, null, "\t"))
+fs.writeFileSync("build.json", JSON.stringify(file_dot, null, "\t"))
+fs.writeFileSync("dist/build.json", JSON.stringify(file_dot, null, "\t"))
+fs.writeFileSync("build/build.json", JSON.stringify(file_hyphen, null, "\t"))
 
 if (process.platform === "win32") {
-	fs.writeFileSync(`build/authme-${build_hyphen}-windows.json`, JSON.stringify(file, null, "\t"))
+	fs.writeFileSync(`build/authme-${build_hyphen}-windows.json`, JSON.stringify(file_dot, null, "\t"))
 } else if (process.platform === "darwin") {
-	fs.writeFileSync(`build/authme-${build_hyphen}-mac.json`, JSON.stringify(file, null, "\t"))
+	fs.writeFileSync(`build/authme-${build_hyphen}-mac.json`, JSON.stringify(file_dot, null, "\t"))
 } else {
-	fs.writeFileSync(`build/authme-${build_hyphen}-linux.json`, JSON.stringify(file, null, "\t"))
+	fs.writeFileSync(`build/authme-${build_hyphen}-linux.json`, JSON.stringify(file_dot, null, "\t"))
 }
