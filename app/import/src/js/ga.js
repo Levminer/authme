c13115cd1f07f3a26a32a353e5e83f8d95d13a00
Levminer <33373714+Levminer@users.noreply.github.com>
2021-07-09 10:14:17 +0200

    Drop Python dependence #121

--- app/import/src/js/ga.js
+++ app/import/src/js/ga.js
@@ -1,6 +1,7 @@
-const ga_import = () => {
+const gaImport = () => {
 	let i = 0
 	let j = 0
+	let string = ""
 	const corrects = []
 	const datas = []
 	let images = []
@@ -16,11 +17,11 @@ const ga_import = () => {
 			images = result.filePaths
 
 			if (canceled === false) {
-				ga_import_resume()
+				gaImportResume()
 			}
 		})
 
-	const ga_import_resume = () => {
+	const gaImportResume = () => {
 		for (i = 0; i < images.length; i++) {
 			const element = images[i]
 
@@ -30,106 +31,94 @@ const ga_import = () => {
 						title: "Authme",
 						buttons: ["Close"],
 						type: "error",
-						message: `
-						No Google Authenticator QR code found on the picture: ${element}.
+						message: `No Google Authenticator QR code found on the picture: ${element}.
 						
-						Try to take a better picture and try again!
-						`,
+						Try to take a better picture and try again!`,
 					})
 
 					return console.warn("Authme - No QR code found (GA)")
 				} else if (res.data.startsWith("otpauth-migration://")) {
-					const python = spawn("python", [python_path, `${res.data}`])
+					// split string
+					const uri = res.data.split("=")
 
-					python.stdout.on("data", (res) => {
-						// correct pictures
-						corrects.push(element)
+					// decode data
+					const data = qr.convert(uri[1])
 
-						// stringify data
-						data = res.toString()
-						console.warn("Authme - Python data recived")
-						datas.push(data)
-
-						j++
-
-						if (images.length == j) {
-							let str = ""
-
-							datas.forEach((element) => {
-								str += element
-							})
-
-							let corrects_str = ""
-							for (let k = 0; k < corrects.length; k++) {
-								if (k === corrects.length - 1) {
-									corrects_str += `${element}.`
-								} else {
-									corrects_str += `${element}, `
-								}
-							}
-
-							dialog
-								.showMessageBox({
-									title: "Authme",
-									buttons: ["Close"],
-									type: "info",
-									defaultId: 0,
-									message: `
-									Google Authenticator QR codes found on these pictures: ${corrects_str}
-									
-									Now select where do you want to save the file!
-									`,
-								})
-								.then(() => {
-									dialog
-										.showSaveDialog({
-											title: "Save import file",
-											filters: [{ name: "Text file", extensions: ["txt"] }],
-											defaultPath: "~/authme_import.txt",
-										})
-										.then((result) => {
-											canceled = result.canceled
-											output = result.filePath
-
-											if (canceled === false) {
-												fs.writeFile(output, str, (err) => {
-													if (err) {
-														console.warn(`Authme - Error creating file - ${err}`)
-													} else {
-														console.warn("Authme - File created")
-													}
-												})
-											}
-										})
-								})
-						}
+					// make a string
+					data.forEach((element) => {
+						const tempsring = `\nName:   ${element.name} \nSecret: ${element.secret} \nIssuer: ${element.issuer} \nType:   OTP_TOTP\n`
+						string += tempsring
 					})
 
-					python.on("error", (err) => {
-						console.log(`Authme - Error executing Python script - ${err}`)
+					// correct pictures
+					corrects.push(element)
 
-						dialog.showMessageBox({
-							title: "Authme",
-							buttons: ["Close"],
-							type: "error",
-							defaultId: 0,
-							message: `
-							Error executing Python script!
-							
-							Please make sure you have Python installed!
-							`,
+					// add to final array
+					datas.push(string)
+
+					j++
+
+					if (images.length == j) {
+						let str = ""
+
+						// make final string
+						datas.forEach((element) => {
+							str += element
 						})
-					})
+
+						// correct pictures string
+						let corrects_str = ""
+						for (let k = 0; k < corrects.length; k++) {
+							if (k === corrects.length - 1) {
+								corrects_str += `${element}.`
+							} else {
+								corrects_str += `${element}, `
+							}
+						}
+
+						dialog
+							.showMessageBox({
+								title: "Authme",
+								buttons: ["Close"],
+								type: "info",
+								defaultId: 0,
+								message: `Google Authenticator QR codes found on these pictures: ${corrects_str}
+								
+								Now select where do you want to save the file!`,
+							})
+							.then(() => {
+								dialog
+									.showSaveDialog({
+										title: "Save import file",
+										filters: [{ name: "Text file", extensions: ["txt"] }],
+										defaultPath: "~/authme_import.txt",
+									})
+									.then((result) => {
+										canceled = result.canceled
+										output = result.filePath
+
+										if (canceled === false) {
+											fs.writeFile(output, str, (err) => {
+												if (err) {
+													console.warn(`Authme - Error creating file - ${err}`)
+												} else {
+													console.warn("Authme - File created")
+												}
+											})
+										} else {
+											return console.warn("Authme - Saveing cancled")
+										}
+									})
+							})
+					}
 				} else {
 					dialog.showMessageBox({
 						title: "Authme",
 						buttons: ["Close"],
 						type: "error",
-						message: `
-						Wrong QR code found on the picture: ${element}.
+						message: `Wrong QR code found on the picture: ${element}.
 						
-						Make sure this is a correct QR code and try again!
-						`,
+						Make sure this is a correct QR code and try again!`,
 					})
 
 					return console.warn("Authme - Wrong QR code found (QR)")
