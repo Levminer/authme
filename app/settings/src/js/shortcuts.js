ef92e33b67b884c89f4d15cbd2a8e34fbf7f7f80
Levminer <33373714+Levminer@users.noreply.github.com>
2021-10-23 16:07:31 +0200

    Fix qucik copy shortcuts

--- app/settings/src/js/shortcuts.js
+++ app/settings/src/js/shortcuts.js
@@ -501,12 +501,12 @@ const generateCodes = () => {
 		</div>
 		<div class="flex justify-center items-center mb-10 mt-5">
 		<button class="buttonr button" id="qs${i}_button_edit" onclick="qsEdit(${i})">
-		<svg id="hk102_svg_edit" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+		<svg id="qs${i}_svg_edit" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 		<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
 		</svg>
 		</button>
-		<button class="buttonr button" id="qs${i}_button_edit" onclick="qsEdit(${i})">
-		<svg id="hk102_svg_delete" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+		<button class="buttonr button" id="qs${i}_button_delete" onclick="qsDelete(${i})">
+		<svg id="qs${i}_svg_delete" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 		<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
 		</svg>
 		</button>
