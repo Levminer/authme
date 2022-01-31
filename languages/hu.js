e110e79b4d5ff6f5304114bf6d3ac0eb96e74bf9
Levminer <33373714+Levminer@users.noreply.github.com>
2022-01-31 17:15:18 +0100

    #177 Hungarian translation

--- languages/hu.js
+++ languages/hu.js
@@ -1,6 +1,10 @@
 /* prettier-ignore */
 /* cSpell:disable */
 module.exports = {
+	"locale": {
+		"code": "hu",
+	},
+
 	"tray": {
         "show_app": "Authme Megjelenítése",
         "hide_app": "Authme Elrejtése",
@@ -32,11 +36,37 @@ module.exports = {
         "info": "Verzió",
     },
 
+	"dialog": {
+		"error": "Hiba történt az Authme futtatása közben.",
+		"integrity": "Nem sikerült a fájlok biztonságának ellenőrzése. \n\nTe vagy valaki más szerkesztette a beállítások fájlt, Authme leállítása biztonsági okokból!",
+		"support": "Authme egy ingyenes, nyílt forráskódú program. \n\nHa tetszik az alkalmazás kérlek támogass!",
+		"feedback": "Köszönöm a visszajelzést! \n\nKérlek a hibákat GitHub-on vagy Email-be jelezd.",
+		"docs": "A dokumentációt a böngészőben tekintheted meg. \n\nKattints a megnyitásra hogy megnyíljon a böngésződben.",
+		"license": "Ez a szoftver a GPL-3.0 licensz alatt érhető el \n\nCopyright © 2020 Lőrik Levente",
+		"no_update_available": "Nem található új frissítés: \n\nA legfrisebb verziót futtatod! \n\nJelenlegi verzió:",
+		"update_error": "Hiba történt az autómatikus frissítés közben. \n\nIndítsd újra az Authme-t és ellenőrizd a hálózati kapcsolatot.",
+	},
+
     "button": {
+		"yes": "Igen",
+		"no": "Nem",
+		"cancel": "Mégse",
 		"close": "Bezárás",
 		"more": "Több",
         "open": "Megnyitás",
         "copy": "Másolás",
+		"copied": "Másolva",
+		"help": "Segítség",
+		"exit": "Kilépés",
+		"report": "Jelentés"
+	},
+
+	"text": {
+		"name": "Név",
+		"code": "Kód",
+		"time": "Idő",
+		"latest_save": "Legfrisebb mentés",
+		"display": "Monitor",
 	},
 
 	"landing": [
@@ -52,4 +82,205 @@ module.exports = {
 		"Ablak rögzítése",
 		"Képernyőképek és az ablak rögzítésének engedélyezése. Aktív amíg újra nem indítod az Atuhme-t.",
 	],
+
+	"landing_dialog": {
+		"no_password": "Biztos vagy benne? \n\nÍgy bárki megnézheti a 2FA kódjaidat akinek hozzáférése van a számítógépedhez!",
+	},
+
+	"landing_text": {
+		"maximum_password": "Maximum 64 karakter lehet a jelszavad hossza!",
+		"minimum_password": "Minimum 8 karakter kell hogy legyen a jelszavad hossza!",
+		"passwords_match":  "A jelszók egyeznek! Kérlek várj!",
+		"top_1000_password": "Ez a jelszó fönt van az 1000 leggyakoribb jelszavak listáján. Kérlek válassz egy biztonságosabb jelszót!",
+		"passwords_dont_match": "A jelszók nem egyeznek! Próbálkozz újra!",
+		"please_wait": "Kérlek várj!",
+	},
+
+    "confirm": [
+		"Jelszó megerősítése",
+		"Add meg a jelszavad a folytatáshoz!",
+		"Jelszó",
+		"Megerősítés",
+		"Több opció",
+		"Ablak rögzítése",
+		"Képernyőképek és az ablak rögzítésének engedélyezése. Aktív amíg újra nem indítod az Atuhme-t."
+	],
+
+	"confirm_text": {
+		"try_again": "Próbálkozz újra!",
+		"locked": "Authme zárolva. Próbálkozz újra 5 másodperc múlva!",
+	},
+
+	"application": [
+		"Authme import fájl választása",
+		"Ha nincs import fájlod hozz létre egyet!",
+		"Fájl választása",
+		"Fájl létrehozása",
+		"Első lépések",
+		"Fájlok importálása",
+		"Online dokumentáció",
+		"Minta import fájl",
+		"Hasznos opciók",
+		"Beállítások",
+		"Támogatás",
+		"Kódok mentése",
+		"Mentsd el a kódjaidat hogy újraindítás után is hozzájuk férj.",
+		"Kódok mentése",
+		"Név",
+		"Leírás",
+	],
+
+	"application_dialog": {
+		"choose_import_file" : "Authme import fájl kiválasztása",
+		"authme_file": "Authme fájl",
+		"old_file_0": "Ez a fájl egy Authme",
+		"old_file_1": "fájl! \n\nVálassz egy Authme export vagy import fájlt!",
+		"codes_saved": "A betöltött 2FA kódjaid mentve! \n\nHa akarsz még kódokat hozzáadni vagy törölni akkor kattints az Eszközökre és Kódok szerkesztése!",
+	},
+
+	"application_text": {
+		"no_results": "Nincs keresési találat!",
+		"not_found_results": "Nincs keresési találat arra hogy"
+	},
+
+	"settings": [
+		"Álltalános",
+		"Kódok",
+		"Gyorsgombok",
+		"Kísérletek",
+		"Visszajelzés",
+		"Névjegy",
+		"Beállítások",
+		"Itt tudod szerkeszteni a beállításokat.",
+		"Ablak bezárása",
+		"Autómatikus indítás",
+		"Authme elindul a számítógépeddel együtt. Az Authme a tálcán fog indulni.",
+		"Tálcára kicsinyítés",
+		"Az Authme bezárásakor nem záródik be. A tálcáról tudod megnyitni az Authme-t.",
+		"Ablak rögzítése",
+		"Képernyőképek és az ablak rögzítésének engedélyezése. Aktív amíg újra nem indítod az Atuhme-t.",
+		"Hardveres gyorsítás",
+		"A GPU használata a jobb élmény érdekébe. Kapcsold be ha laggokat vagy akadásokat tapasztalsz.",
+		"Nyelv",
+		"Válaszd ki a nyelvet amin az Authme megjelenik. A fordítások nem tökéletesek és a nyelvváltáshoz újraindítás szükséges.",
+		"Alapértelmezett monitor",
+		"Válaszd ki melyik képernyőn nyílik meg az Authme. Ha a kiválasztott monitor nem elérhető, a fő monitoron fog megnyílni az Authme.",
+		"Adatok törlése",
+		"A jelszavak, a 2FA kódjaid, valamint a beállításaid törlődnek. Légy óvatos.",
+		"Adatok törlése",
+		"Mappák megnyitása",
+		"Megnyithatod az Authme álltal használt mappákat. A fájlok törlése vagy szerkesztése problémákat okozhat.",
+		"Authme mappa",
+		"Beállítások mappa",
+		"Cache mappa",
+		"Logok",
+		"Megnézheted a logokat ha hibákat tapasztalsz. Az összes logot a beállítások mappába találod.",
+		"Legújabb log",
+		"Log mappa",
+		"Kódok leírása",
+		"A mentett kódok leírása látható lesz. Kattintás után másolhatod a leírást.",
+		"Kódok elhomályosítása",
+		"A megjelenített kódok elhomályosítása. Ugyanúgy kitudod másolni vagy megjeleníteni a kódokat ha föléviszed az egeret.",
+		"Keresési előzmények",
+		"A legutóbbi keresés mentése. Újraindítás után is működik.",
+		"Keresés törlése másolás után",
+		"A keresőmező törlése keresés után. Hasznos ha sok kódot másolsz.",
+		"Gyors másolás gyorsgombok",
+		"Itt tudod módosítani a gyors másolás gyorsgombokat, olvasd el a dokumentációt több infóért.. A gyorsgombok nem működnek ezen a lapon, menj át egy mások beállítás lapra hogy aktiváld azokat!",
+		"Gyorsgombok",
+		"Itt tudod módosítani a gyorsgombokat, olvasd el a dokumentációt több infóért. A gyorsgombok nem működnek ezen a lapon, menj át egy mások beállítás lapra hogy aktiváld azokat!",
+		"Globális gyorsgombok",
+		"Itt tudod módosítani a globális gyorsgombokat, olvasd el a dokumentációt több infóért. A gyorsgombok nem működnek ezen a lapon, menj át egy mások beállítás lapra hogy aktiváld azokat!",
+    ],
+
+	"settings_dialog": {
+		"restart": "A beállítás megváltoztatásához újra kell indítani az alkalmazást. \n\nÚjra akarod indítani most?",
+		"clear_data": "Biztos törölni akarsz minden adatot? \n\nEzt nem lehet visszavonni!",
+		"confirm_clear_data": "Teljesen biztos vagy benne? \n\nNincs visszaút!",
+	},
+
+	"import": [
+		"Az import fájlodat létre tudod hozni 2FA QR-kódokból és Google Hitelesítő QR-kódokból.",
+		"Ablak bezárása",
+		"QR kód",
+		"Használat",
+		"Készíts képernyőképeket a QR-kódokról, mentsd őket le a számítógépedre, és válaszd ki a képeket.",
+		"Ha segítségre lenne szükséged:",
+		"Részletes leírás",
+		"Kép(ek) kiválasztása",
+		"Webkamera",
+		"Google Hitelesítő QR kód",
+		"Használat",
+		"Nyomj a három pontra a jobb felső sarokba, exportáld a fiókod, fényképezd le a  QR-kódokat, és válaszd ki a képeket amiket átmentettél a számítógépedre.",
+		"Ha segítségre lenne szükséged:",
+		"Részletes leírás",
+		"Kép(ek) kiválasztása",
+		"Webkamera",
+    ],
+
+	"import_dialog": {
+		"import_qrcode": "Importálás QR kód-okból",
+		"image_file": "Kép fájl",
+		"save_file": "Fájl mentése",
+		"no_webcam": "Nem található webkamera! \n\nEllenőrizd hogy a webkamerád megfelelően működik vagy nem használja más alkamazás.",
+		"wrong_qrcode": "Rossz féle QR kód találva a kamerán! \n\nGyőződj meg róla hogy ez egy jó QR kód!",
+		"qrcode_found": "QR kód(ok) sikeresen scannelve! \n\nVálaszd ki hova akarod menteni a fájlt!",
+		"webcam_used": "Webkamera használatban! \n\nEllenőrizd hogy a webkamerád megfelelően működik vagy nem használja más alkamazás.",
+		"import_qa_qrcode": "Importálás Google Hitelesítő QR kód-okból",
+		"no_qrcode_found_0": "Nem található QR kód a képen:",
+		"no_qrcode_found_1": "\n\nPróbálj készíteni egy jobb képet.",
+		"correct_qrcode_found_0": "QR kód scannelve ezekről a képekről:",
+		"correct_qrcode_found_1": "\n\nVálaszd ki hova akarod menteni a fájlt!",
+		"wrong_qrcode_found_0": "Rossz féle QR kód találva a képen:",
+		"wrong_qrcode_found_1": "\n\nGyőződj meg róla hogy ez egy jó QR kód!",
+	},
+
+	"export": [
+		"Itt exportálhatod az elmentett 2FA kódjaidat.",
+		"Ablak bezárása",
+		"Kódok exportálása",
+		"Ha elmentetted a kódjaidat akkor exportálhatod a QR-kódokat és azok secret-ét.",
+		"Exportált kódok mentése",
+		"Mentsd el a 2FA kódjaidat hogy máshol is tudd azokat használni.",
+		".authme fájl",
+		"Ideális ha az Authme-be vagy más Authme alkalmzásba akarsz importálni.",
+		".html fájl",
+		"Ideális ha be akarod olvasni a QR-kódokat vagy biztonsági mentésnek.",
+	],
+
+	"export_dialog": {
+		"text_file": "Szöveges fájl",
+		"html_file": "HTML fájl",
+		"no_save_found": "Nincsenek mentett kódok. \n\nMenj vissza a főoldalra és mentsd el a kódjaidat!",
+	},
+
+	"edit": [
+		"Kódok szerkesztése",
+		"Itt tudod szerkeszteni vagy törölni a mentett kódjaidat vagy új kódokat hozzáadni.",
+		"Ablak bezárása",
+		"Visszaállítás",
+		"Vissza tudod állítani az előző mentést. Ha lentebb betöltöd a kódjaidat az felül fogja írni a jelenlegi mentést.",
+		"Visszaállítás",
+		"Kódok betöltése",
+		"Be tutod tölteni a mentett kódjaidat. Ezután tudod szerkeszteni azokat, vagy újakat hozzáadni.",
+		"Kódok betöltése",
+		"Szerkesztő mód",
+		"Most már tudod szerkeszteni a kódjaidat, vagy újakat hozzáadni.",
+		"Mentés",
+		"Kódok hozzáadása",
+		"Több opció",
+		"Visszaállítás",
+		"Kódok törlése",
+	],
+
+	"edit_dialog": {
+		"load_rollback": "Biztos vissza akarod állítani a legfrisebb mentést? \n\nEz a mentés fölülfogja írni a mentett kódjaidat!",
+		"rollback_successful": "Visszaállítás sikeres! \n\nMenj vissza a főoldalra hogy megtekintsd a kódjaidat!",
+		"delete_code": "Biztos ki akarod törölni ezt a kódot? \n\nA művelet visszavonásához kattints a több opcióra és visszaállítás.",
+		"create_save": "Biztos menteni akarod a változtatásokat? \n\nEz fölülfogja írni a jelenlegi kódjaidat!",
+		"codes_added": "Kódok hozzáadva. \n\nGörgess le!",
+		"load_codes": "Biztos be akarod tölteni a jelenleg mentett kódjaidat? \n\nEz felül fogja írni a jelenlegi mentést!",
+		"revert_changes": "Biztos vissza akarod vonni az összes változtatást? \n\nEzzel elveszted az összes módosítást!",
+		"delete_all_codes": "Biztos ki akarod törölni az össszes kódot? \n\nA visszaállításhoz töltsd be a legutolsó mentést.",
+	},
 }
