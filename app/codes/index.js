310b2493ab9661545ff8f3887f63ee22f4f6581f
Levminer <33373714+Levminer@users.noreply.github.com>
2022-05-25 18:19:16 +0200

    Update packages

--- app/codes/index.js
+++ app/codes/index.js
@@ -228,7 +228,7 @@ const generateCodeElements = (data) => {
 						<div id="progress${i}" class="progressFill"></div>
 					</div>
 					<div class="flex flex-col justify-center items-center pt-6">
-						<button onclick="copyCode(${i})" id="copy${i}" class="buttoni w-[170px] mb-7">
+						<button onclick="copyCode(${i})" id="copy${i}" class="buttoni w-[180px] mb-7">
 							<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
 							</svg>
