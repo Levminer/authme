9b11110a272c51f2c130e295ad756f618d1fbfbb
Lőrik Levente <levminer@levminer.com>
2023-05-09 17:40:22 +0200

    Import page wording improvements

--- interface/components/details.svelte
+++ interface/components/details.svelte
@@ -1,5 +1,5 @@
 <div class="transparent-800 mx-auto w-full rounded-xl">
-	<Disclosure let:open>
+	<Disclosure let:open defaultOpen={openByDefault}>
 		<DisclosureButton class="transparentHover flex w-full items-center justify-between rounded-xl px-6 py-3 text-left text-2xl font-medium text-white outline-none duration-200 ease-linear focus:outline-none focus-visible:ring-4 focus-visible:ring-purple-500">
 			<span>More options</span>
 			<svg xmlns="http://www.w3.org/2000/svg" class={`${open ? "rotate-180 transform" : ""} h-5 w-5 text-white`} fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
@@ -7,7 +7,7 @@
 			</svg>
 		</DisclosureButton>
 		<Transition enter="transition duration-200 ease-out" enterFrom="transform scale-95 opacity-0" enterTo="transform scale-100 opacity-100" leave="transition duration-200 ease-out" leaveFrom="transform scale-100 opacity-100" leaveTo="transform scale-95 opacity-0">
-			<DisclosurePanel class="px-4 pt-4 pb-2 text-left text-lg text-gray-200">
+			<DisclosurePanel class="px-4 pb-2 pt-4 text-left text-lg text-gray-200">
 				<slot />
 			</DisclosurePanel>
 		</Transition>
@@ -16,4 +16,5 @@
 
 <script lang="ts">
 	import { Disclosure, DisclosureButton, DisclosurePanel, Transition } from "@rgossiaux/svelte-headlessui"
+	export let openByDefault = false
 </script>
