2a116673801d1c462da47426d5d43308e2df5a25
Lőrik Levente <levminer@levminer.com>
2024-01-08 14:19:36 +0100

    Update rename.js

--- scripts/rename.js
+++ scripts/rename.js
@@ -19,11 +19,11 @@ if (os === "win32") {
 	}
 } else if (os === "darwin") {
 	try {
-		copyFileSync(`./core/target/universal-apple-darwin/release/bundle/dmg/Authme_${version}_x64.dmg`, `./core/target/release/upload/authme-${version}-macos-x64.dmg`)
+		copyFileSync(`./core/target/universal-apple-darwin/release/bundle/dmg/Authme_${version}_universal.dmg`, `./core/target/release/upload/authme-${version}-macos-x64.dmg`)
 		copyFileSync("./core/target/universal-apple-darwin/release/bundle/macos/Authme.app.tar.gz", `./core/target/release/upload/authme-${version}-macos-x64.tar.gz`)
 		copyFileSync("./core/target/universal-apple-darwin/release/bundle/macos/Authme.app.tar.gz.sig", `./core/target/release/upload/authme-${version}-macos-x64.sig`)
 	} catch (err) {
-		console.log("File not found")
+		console.log("File not found", err)
 	}
 } else {
 	try {
