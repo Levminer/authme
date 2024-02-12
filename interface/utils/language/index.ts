88e76f4d4476bd1a414d7e2cec331cf3f97165d4
Lőrik Levente <levminer@levminer.com>
2024-02-12 10:18:53 +0100

    Select language automatically

--- interface/utils/language/index.ts
+++ interface/utils/language/index.ts
@@ -1,10 +1,14 @@
 import { localeEN } from "@utils/language/en"
+import { localeHU } from "@utils/language/hu"
+import { localeES } from "@utils/language/es"
 
 export const getLanguage = () => {
 	const language = navigator.language
 
-	if (language.startsWith("en")) {
-		return localeEN
+	if (language.startsWith("es")) {
+		return localeES
+	} else if (language.startsWith("hu")) {
+		return localeHU
 	} else {
 		return localeEN
 	}
