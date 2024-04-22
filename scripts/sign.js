e008cd80ad0125a0e42e2d8851ff8abda0d3b03f
Lőrik Levente <levminer@levminer.com>
2024-04-22 14:48:13 +0200

    Update actions

--- scripts/sign.js
+++ scripts/sign.js
@@ -2,7 +2,7 @@ import { exec } from "child_process"
 import { platform } from "os"
 
 if (platform() === "win32") {
-	exec("trusted-signing-cli -f ./core/target/release/Authme.exe -e https://eus.codesigning.azure.net -c mnr -n Profile1", (error, stdout) => {
+	exec("trusted-signing-cli ./core/target/release/Authme.exe -e https://eus.codesigning.azure.net -a mnr -c Profile1", (error, stdout) => {
 		if (error) {
 			return console.error(`Error: ${error}`)
 		}
